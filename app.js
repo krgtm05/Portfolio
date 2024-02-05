e3bc32e848bff90a0fd4a567af5154bbecff30d2
Gautam Kumar <krgautamofficial@gmail.com>
2024-02-06 00:40:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,147 @@
+const scrolltopbtn = document.querySelector(".top");
+const formId = document.querySelector("#form");
+
+
+//text effect
+document.addEventListener('DOMContentLoaded', function() {
+  const originalText = "Gautam Kumar";
+  let animatedTextElement = document.querySelector('#animatedText');
+  const textArray = originalText.split('');
+  let charIndex = 0;
+  function animateText() {
+    animatedTextElement.textContent += textArray[charIndex];
+    charIndex++;
+    if (charIndex < textArray.length) {
+      setTimeout(animateText,150); // Adjust the delay between characters
+    }
+  }
+  animateText();
+});
+//Project Menu list
+projectMenuArray.forEach((item,idx)=>{
+  const name = document.createElement('li');
+  name.classList.add('cert');
+  name.innerText = item;
+  // console.log(item);
+  if(idx == 5){
+    const ele = document.createElement('p');
+    ele.innerText = 'Fun Projects';
+    document.querySelector("#project-menu ul").append(ele);
+  }
+  document.querySelector('#project-menu ul').append(name);
+});
+
+//ACTIVE CHANGING CARD ON SELECT 
+const menuOptions = document.querySelectorAll('#project-menu ul li');
+menuOptions[0].classList.add('active');
+menuOptions.forEach((el,idx)=>{
+  el.addEventListener("click",()=>{
+    const effect = document.querySelector('.project-div');
+    effect.classList.add('animation');
+    setTimeout(()=>{
+      effect.classList.remove('animation');
+    },500);
+    const item = projectCardArray[idx];
+    menuOptions.forEach((ele)=>{
+      ele.classList.remove('active');
+    })
+    el.classList.add('active');
+    document.querySelector(".pjt-name").innerText = item.name;
+    document.querySelector(".project-div-header a").href = item.src;
+    document.querySelector(".pjt-about").innerText = item.about;
+    const parentDiv = document.querySelector('.project-techstack-btn');
+      while (parentDiv.firstChild) {
+        parentDiv.removeChild(parentDiv.firstChild);
+      }
+    item.techStack.forEach((e)=>{
+         const techtbn = document.createElement('button');
+         techtbn.classList.add('pjt-techs');
+         techtbn.innerText = e;
+         const parent = document.querySelector('.project-techstack-btn');
+         parent.append(techtbn);
+    });
+    const pjtStatus  = document.querySelector(".pjt-status");
+    if(item.status === 1){
+      pjtStatus.innerText = '⦿ Completed'
+      pjtStatus.style.color = 'green';
+    }
+    else{
+      pjtStatus.innerText = '⦿ In Progress'
+      pjtStatus.style.color = 'red';
+      
+    }
+    document.querySelector(".project-foot a").href = item.codeSrc;
+    
+  });
+  
+});
+
+//Adding certificates
+certificateArray.forEach((item)=>{
+  const certDiv = document.createElement("div");
+  const certfName = document.createElement("p");
+  const certSrc = document.createElement("a");
+  certSrc.innerHTML = '<i class="fa-regular fa-eye"></i>';
+
+  certDiv.classList.add("cert");
+  certfName.innerText=item.certificateName;
+  certSrc.href = item.src;
+  certSrc.target = '__blank';
+  certDiv.append(certfName,certSrc);
+  document.querySelector(".six-section").appendChild(certDiv);
+});
+
+//from submission
+formId.addEventListener('submit',async (event)=>{
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    var emailInput = document.getElementById('email');
+    var email = emailInput.value;
+    // Basic email format validation
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      alert('Invalid email format. Please enter a valid email address.');
+      return;
+    }
+    else
+        alert("Thanks for your response. Message sent succesfully");
+          // showMessageModel("sddfghj","kjhgfd");
+        document.getElementById("form").reset();
+        //doing some unethical task just because working hehehe. to avoid redirection to the form provider page here im not handeling promise right way :)
+        await fetch('https://formspree.io/f/xdoqadwr',{
+            method:"POST",
+            body:formData
+        });
+});
+
+//open model on form submission
+function showMessageModel(message, type) {
+    // Get the modal
+    const modal = document.getElementById('myModal');
+
+    // Set message and style based on the type
+    modal.innerHTML = message;
+    modal.style.backgroundColor = type === 'success' ? '#4CAF50' : '#F44336';
+    modal.style.color = '#fff';
+
+    // Show the modal
+    modal.style.display = 'block';
+
+    // Hide the modal after 2 seconds
+    setTimeout(() => {
+      modal.style.display = 'none';
+    }, 2000);
+}
+
+// scrolltotop implementation
+window.onscroll =() =>{
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrolltopbtn.style.display = "block";
+      } else {
+        scrolltopbtn.style.display = "none";
+      }
+};
+scrolltopbtn.addEventListener("click",()=>{
+      document.body.scrollTop = 0; //safari
+      document.documentElement.scrollTop = 0;//chrome and other
+});
---
+++ data.js
@@ -0,0 +1,111 @@
+const certificateArray = [
+   
+   {
+    certificateName :'JavaScript - Mastering the Fundamentals',
+    src:'https://moonshot.scaler.com/s/sl/nIWfuBHpD8'
+   },
+   {
+    certificateName :'HackerRank Skill Certificate ( Angular )',
+    src:'https://www.hackerrank.com/certificates/a37d31cb7521'
+   },
+   {
+    certificateName :'HackerRank Skill Certificate ( Problem Solving )',
+    src:'https://www.hackerrank.com/certificates/9da2821298c8'
+   },
+   {
+    certificateName:'Frontend Fundamentals',
+    src:'https://www.pirple.com/certificates/jzr6tdbshi'
+   },
+   {
+    certificateName :'Full Stack Web  development',
+    src:'https://trainings.internshala.com/view_certificate/852035AD-AB8F-B94C-BD86-2B009682A52C/2E7FAB42-9842-D293-F37C-C38B696F693F/'
+   }
+   
+];
+
+const projectMenuArray = [
+    'Real Time Currecny Converter',
+    'My Portfolio',
+    'The Weather App',
+    'TextUtils',
+    'Todo App',
+    'Tic Tac Toe',
+    'Rock Paper Scissor',
+    'SnakeBlitz',
+    'Calculator Web App'
+];
+
+const projectCardArray = [
+    {
+        name :'Real Time Currency Converter',
+        src:'https://krgtm05.github.io/KurrencyKonverter/',
+        techStack : ['JavaScript','HTML','CSS','APIs'],
+        about: 'It is a simple and efficient web application that enables users to convert currencies in real-time. With up-to-date exchange rates, users can effortlessly perform currency conversions.',
+        codeSrc:'https://github.com/krgtm05/KurrencyKonverter/tree/master',
+        status: 1
+    },
+    {
+        name :'My Portfolio - Gautam Kumar',
+        src:'https://krgtm05.github.io/gautam.in/#home',
+        techStack : ['HTML','CSS','JavaScript'],
+        about:'Crafted an engaging portfolio development website showcasing my skills and projects, leveraging HTML, CSS, and JavaScript for a seamless and interactive user experience.',
+        codeSrc:'dfghjkdfghjkfghj',
+        status: 0
+    },
+    {
+        name :'The Weather App',
+        src:'https://krgtm05.github.io/AngularWeatherApp/',
+        techStack : ['Angular','JavaScript','API','HTML','CSS'],
+        about: 'I used the Angular Framework to create the UI of the weather applicaion. I used open weather api to fetch real time weather details of any location.',
+        codeSrc:'https://github.com/krgtm05/AngularWeatherApp',
+        status: 1
+    },
+    {
+        name :'TextUtils',
+        src:'https://krgtm05.github.io/TextUtils/',
+        techStack : ['React','Bootstrap','HTML','JavaScript',],
+        about: 'TextUtils is a text utility project which can perform operations like lowercase, uppercase, capitalize, copy to clipboard operations on text.',
+        codeSrc:'https://github.com/krgtm05/TextUtils',
+        status: 1
+    },
+    {
+        name :'Todo App',
+        src:'https://krgtm05.github.io/Task-List/',
+        techStack : ['HTML','CSS','JavaScript'],
+        about: 'ToDo App is a place to manage all your task datewise that make you sure to not forget any necessary task.',
+        codeSrc:'https://github.com/krgtm05/Task-List',
+        status: 1
+    },
+    {
+        name :'Tic Tac Toe',
+        src:'https://krgtm05.github.io/tictactoe.js/',
+        techStack : ['JavaScript','HTML','CSS'],
+        about: 'Play an exciting game of Tic Tac Toe, challenging your strategic skills in a classic and timeless battle for victory.',
+        codeSrc:'https://github.com/krgtm05/tictactoe.js',
+        status: 1
+    },
+    {
+        name :'Rock Paper Scissor',
+        src:'https://krgtm05.github.io/rockpaperscissorgame/',
+        techStack : ['JavaScript','HTML','CSS'],
+        about: 'Experience the timeless fun of Rock, Paper, Scissors with a digital twist, featuring engaging gameplay and dynamic outcomes.',
+        codeSrc:'https://github.com/krgtm05/rockpaperscissorgame',
+        status: 1
+    },
+    {
+        name :'SnakeBlitz',
+        src:'https://krgtm05.github.io/SnakeBlitz/',
+        techStack : ['HTML','CSS','JavaScript'],
+        about: 'Enjoy a classic Snake game with score tracking and a leaderboard feature to record both current scores and highest achievements.',
+        codeSrc:'https://github.com/krgtm05/SnakeBlitz',
+        status: 1
+    },
+    {
+        name :'Calculator Web App',
+        src:'https://krgtm05.github.io/KalKulator/',
+        techStack : ['HTML','CSS','JavaScript'],
+        about: "I'm a calculator, That's It !",
+        codeSrc:'https://github.com/krgtm05/KalKulator',
+        status: 1
+    }
+]
